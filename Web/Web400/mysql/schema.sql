a01827a0d781250c9a4a412274611248b9183b85
DS-KoolAid <matthew.jackoski@gmail.com>
2021-04-07 08:23:11 -0700

    adding inital Web500 - still needs redesign and to proof if it can be solved

---
+++ Web/Web400/mysql/schema.sql
@@ -0,0 +1,20 @@
+USE silicon;
+
+CREATE TABLE login_table (
+name char(64),
+pw_hash char(64)
+);
+
+CREATE TABLE flag_t (
+id int,
+flag char(64)
+);
+
+
+    INSERT INTO login_table VALUES ('admin','2fdde255d49fe7fc6f3323614bb55994c55db86142df93c2e4cb11570521fe54');
+    INSERT INTO flag_t VALUES (1,'flag{Type_Juggling_f0r_th3_w1n}');
+
+
+
+-- Full hash: 2fdde255d49fe7fc6f3323614bb55994c55db86142df93c2e4cb11570521fe54
+-- Actual password: nHMx.z3iYN*A7WsGJCH
---
+++ Web/Web400/src/src/db.php
@@ -0,0 +1,51 @@
+<?php
+
+// error_reporting(0);
+require_once("config.php"); // DB config
+
+
+
+$db = new mysqli($MYSQL_HOST, $MYSQL_USERNAME, $MYSQL_PASSWORD, $MYSQL_DBNAME);
+
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+  }
+
+  
+  function check_errors($var) {
+    if ($var === false) {
+      die("Error. Please contact CTF administrator. (seems to be a db problem)");
+    }
+  }
+
+function retreive_user_hash($uname){
+
+    global $db;
+    $statement = $db->prepare(
+      "SELECT pw_hash FROM login_table WHERE name = ?"
+    );
+    check_errors($statement);
+    $statement->bind_param("s", $uname);
+    check_errors($statement->execute());
+    $res = $statement->get_result();
+    check_errors($res);
+    $pw_hash = $res->fetch_assoc();
+    $statement->close();
+    return $pw_hash;
+
+}
+
+function load_flag(){
+    global $db;
+    $statement = $db->prepare(
+      "SELECT flag FROM flag_t WHERE id=1"
+    );
+    check_errors($statement);
+    $statement->bind_param("s", $uname);
+    check_errors($statement->execute());
+    $res = $statement->get_result();
+    check_errors($res);
+    $flag = $res->fetch_assoc();
+    $statement->close();
+    return $flag;
+}
